0a71fac3ba46525a9a3e70977a261be84359571e
Simon BRAMI <simon.brami@epitech.eu>
2017-12-11 14:17:32 +0100

    Login View

---
+++ SportCitizen/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  SportCitizen
+//
+//  Created by Simon BRAMI on 11/12/2017.
+//  Copyright © 2017 Simon BRAMI. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
