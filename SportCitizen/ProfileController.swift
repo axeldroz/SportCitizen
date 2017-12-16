cea52023a678202d20ba17f9f160255afa60e913
Axel Drozdzynski <axeldroz@hotmail.fr>
2017-12-16 10:41:42 +0100

    Profile View : UX Design

--- SportCitizen/ProfileController.swift
+++ SportCitizen/ProfileController.swift
@@ -2,14 +2,18 @@
 //  ProfileController.swift
 //  SportCitizen
 //
-//  Created by Simon BRAMI on 13/12/2017.
-//  Copyright © 2017 Simon BRAMI. All rights reserved.
+//  Created by Axel DROZDZYNSKI on 13/12/2017.
+//  Copyright © 2017 Axel DROZDZYNSKI. All rights reserved.
 //
 
 import UIKit
 
 class ProfileController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var bioDescr: UITextView!
+    @IBOutlet weak var favoriteSport: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
