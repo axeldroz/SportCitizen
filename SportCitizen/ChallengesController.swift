54f93fbe890026a91a06749709bf6d703da115f1
Simon BRAMI <simon.brami@epitech.eu>
2017-12-16 17:19:20 +0100

    commit

--- SportCitizen/ChallengesController.swift
+++ SportCitizen/ChallengesController.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class ChallengesController: UINavigationController{
-
+class ChallengesController: UINavigationController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
