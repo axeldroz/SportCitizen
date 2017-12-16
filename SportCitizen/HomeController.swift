6bd200bc51f0e33bf8e056b676680cf1dc7f8c1b
Simon BRAMI <simon.brami@epitech.eu>
2017-12-16 16:39:47 +0100

    Architecture modified -> Nav Controller and Tab bar

--- SportCitizen/HomeController.swift
+++ SportCitizen/HomeController.swift
@@ -7,31 +7,14 @@
 
 import UIKit
 
-class HomeController: UITabBarController, UITabBarControllerDelegate {
+class HomeController: UITabBarController {
 
-    @IBOutlet weak var NavigationBar: UINavigationItem!
-
-    
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        let tabBarIndex = tabBarController.selectedIndex
-        if tabBarIndex == 0 {
-            tabBarController.title = "Challenges"
-        }
-        else if tabBarIndex == 1 {
-            tabBarController.title = "Feed"
-        }
-        else if tabBarIndex == 2 {
-            tabBarController.title = "Profile"
-        }
-    }
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.delegate = self
         let tabBarItems = tabBar.items! as [UITabBarItem]
         tabBarItems[0].imageInsets = UIEdgeInsetsMake(6,0,-6,0)
         tabBarItems[1].imageInsets = UIEdgeInsetsMake(6,0,-6,0)
+        tabBarItems[2].imageInsets = UIEdgeInsetsMake(6,0,-6,0)
 
 
         self.selectedIndex = 1
